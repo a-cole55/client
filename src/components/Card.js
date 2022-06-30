158caf23e617d5d0042b0df4b84ec7ab9fae7b14
a-cole55 <arniqua_c@yahoo.com>
2022-06-29 20:53:05 -0500

    initial commit

---
+++ src/components/Card.js
@@ -0,0 +1,9 @@
+export default function Card(){
+    return (
+        <div className="card">
+        <h6>Date</h6>
+        <input placeholder="Write your ___ here"></input>
+        <button type="submit">Submit</button>
+      </div>
+    )
+}
