7ec0f6a622eb24644f98a021dffcf0ce94375e4e
a-cole55 <arniqua_c@yahoo.com>
2022-07-01 17:14:42 -0500

    added sticky note styling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,72 @@
-import Card from "./components/Card";
+import { useState, useEffect } from "react";
+import Axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Anonymity</h1>
-      </header>
-        <div className="anonymous">
-          <Card />
-        </div>
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [name, setName] = useState("");
+  const [listOfPosts, setListofPosts] = useState([]);
+
+  const addPost = () => {
+    Axios.post("http://localhost:4000/addPost", {
+      name: name, 
+      title: title,
+      description: description
+    }).then(()=> {
+      alert("Whoohoo! Success!")
+    })
+    .catch(() => {
+      console.error()
+    });
+  };
+
+  useEffect(() => {
+    Axios.get("http://localhost:4000/read", {
+    }).then((response)=> {
+      setListofPosts(response.data)
+    })
+    .catch(() => {
+      console.error()
+    });
+}, []);
+
+    return (
+        <div className="App">
+          <h1>Anonymity</h1>
+        <div className="addPost">
+        <input 
+            type= "text" 
+            placeholder="Title **Not Required**" 
+            onChange={(event) => {
+              setTitle(event.target.value);
+            }}
+            />
+          <textarea className="description"
+            type= "text" 
+            placeholder="Write your text here" 
+            onChange={(event) => {
+              setDescription(event.target.value);
+            }}
+            />
+          <input 
+            type= "text" 
+            placeholder="anon name **Not Required**" 
+            onChange={(event) => {
+              setName(event.target.value);
+            }}
+            />
+          <button type="submit" onClick={addPost}>Submit</button>
+      </div>
+      <div className="posts">
+        {listOfPosts.map((val) => {
+          return <div className="stickyNotes">  
+            <h2>Title</h2>
+            <p>{val.postDescription}</p>
+            <h3>{val.name}</h3>
+            <h6>Date</h6> 
+            </div>
+      })}
+      </div>
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,41 @@
-export default function Card(){
-    return (
-        <div className="card">
-        <h6>Date</h6>
-        <input placeholder="Write your ___ here"></input>
-        <button type="submit">Submit</button>
-      </div>
-    )
-}
+// import { useState ,useEffect} from "react";
+// import Axios from 'axios';
+
+// export default function Card(){
+//   const [description, setDescription] = useState("");
+//   const [name, setName] = useState("");
+//   const [listOfPosts, setListofPosts] = useState([])
+
+//   const addPost = () => {
+//     Axios.post("http://localhost:4000/addPost", {
+//       name: name, 
+//       description: description
+//     }).then(()=> {
+//       alert("Whoohoo! Success!")
+//     })
+//     .catch(() => {
+//       console.error()
+//     });
+//   }
+
+//     return (
+//         <div className="card">
+//         <h6>Date</h6>
+//         <textarea className="description"
+//           type= "text" 
+//           placeholder="Write your text here" 
+//           onChange={(event) => {
+//             setDescription(event.target.value);
+//           }}
+//           />
+//         <input 
+//           type= "text" 
+//           placeholder="anon name **Not Required**" 
+//           onChange={(event) => {
+//             setName(event.target.value);
+//           }}
+//           />
+//         <button type="submit" onClick={addPost}>Submit</button>
+//       </div>
+//     )
+// }
