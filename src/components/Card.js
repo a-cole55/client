4f945619f4d6369bd186c00b3b08c94660dc6a2d
Nora Vargas <noravgs92@gmail.com>
2022-07-01 15:01:07 -0500

    added sticky notes html and css

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,14 @@
 export default function Card(){
     return (
         <div className="card">
-        <h6>Date</h6>
-        <input placeholder="Write your ___ here"></input>
+        <ul>
+  <li>
+    <a href="#" contenteditable>
+      <h2>Title #1</h2>
+      <p>Text Content #1</p>
+    </a>
+  </li>
+  </ul>
         <button type="submit">Submit</button>
       </div>
     )
